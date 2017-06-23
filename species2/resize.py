e2e32993123f59517fdbdaaa69a2bf5c71b16c3f
Xiaoliang Li <xiaoliangl@vmware.com>
2017-06-23 23:05:20 +0800

    using own path

--- species2/resize.py
+++ species2/resize.py
@@ -1,7 +1,9 @@
-import os, shutil, glob, sys
-import pandas as pd
-import numpy as np
+import glob
+import os
+
 import cv2
+
+from settings import *
 
 
 def resize(src_dir, tgt_dir, tgt_size):
@@ -9,15 +11,16 @@ def resize(src_dir, tgt_dir, tgt_size):
     files = glob.glob('*.jpg')
 
     for f in files:
-        fn = src_dir+'/'+f
-        tgt_fn = tgt_dir+'/'+f
+        fn = src_dir + '/' + f
+        tgt_fn = tgt_dir + '/' + f
         print(fn)
         print(tgt_fn)
         img = cv2.imread(fn)
         res = cv2.resize(img, tgt_size)
-        #region = res[50:450, 50:450]
-        #gray = cv2.cvtColor(region, cv2.COLOR_BGR2GRAY)
+        # region = res[50:450, 50:450]
+        # gray = cv2.cvtColor(region, cv2.COLOR_BGR2GRAY)
         cv2.imwrite(tgt_fn, res)
-        
-resize('/home/chicm/ml/kgdata/species/train', '/home/chicm/ml/kgdata/species/train-640', (640,640))
-resize('/home/chicm/ml/kgdata/species/test', '/home/chicm/ml/kgdata/species/test-640', (640,640))
+
+
+resize(DATA_DIR + '/train', TRAIN_DIR, (640, 640))
+resize(DATA_DIR + '/test', TEST_DIR, (640, 640))
--- species2/settings.py
+++ species2/settings.py
@@ -1,4 +1,4 @@
-DATA_DIR = '/home/chicm/ml/kgdata/species'
+DATA_DIR = '/home/caitsithx/deeplearning_workspace/data/invasive-species-monitoring'
 TRAIN_DIR = DATA_DIR + '/train-640'
 TEST_DIR = DATA_DIR + '/test-640'
 MODEL_DIR = DATA_DIR + '/models'

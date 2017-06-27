e3317daeed4a32700a68f73a4a84ff39c83bf189
Xiaoliang Li <xiaoliangl@vmware.com>
2017-06-27 22:09:02 +0800

    error path

--- species2/settings.py
+++ species2/settings.py
@@ -1,5 +1,5 @@
 DATA_DIR = '/home/caitsithx/deeplearning_workspace/data/invasive-species' \
-           '-monitoring '
+           '-monitoring'
 TRAIN_DIR = DATA_DIR + '/train-640'
 TEST_DIR = DATA_DIR + '/test-640'
 MODEL_DIR = DATA_DIR + '/models'

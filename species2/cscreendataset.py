47ba6bd8107acfd065f1910bcf29391c5c91789f
chicm <idv2005@gmail.com>
2017-06-29 17:38:16 +0800

    updates

--- species2/cscreendataset.py
+++ species2/cscreendataset.py
@@ -89,7 +89,8 @@ data_transforms = {
         transforms.RandomHorizontalFlip(),
         transforms.Lambda(lambda x: randomRotate(x)),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
     ]),
     'trainv3': transforms.Compose([
         # transforms.Scale(299),
@@ -97,7 +98,8 @@ data_transforms = {
         transforms.RandomHorizontalFlip(),
         transforms.Lambda(lambda x: randomRotate(x)),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
     ]),
     'valid': transforms.Compose([
         transforms.Scale(224),
@@ -109,7 +111,8 @@ data_transforms = {
         transforms.Scale(299),
         # transforms.CenterCrop(299),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
     ]),
     'test': transforms.Compose([
         transforms.Scale(224),
@@ -137,51 +140,42 @@ dset_classes = dsets['train'].classes
 save_array(CLASSES_FILE, dset_classes)
 '''
 
-
-def get_train_loader(model, batch_size=16, shuffle=True):
-    if model.name == 'inceptionv3':
+def get_train_loader(model, batch_size = 16, shuffle = True):
+    if model.name.startswith('inception'):
         transkey = 'trainv3'
     else:
         transkey = 'train'
     if hasattr(model, 'batch_size'):
         batch_size = model.batch_size
-    # train_v2.csv
-    dset = PlanetDataset(DATA_DIR + '/train_labels.csv',
-                         transform=data_transforms[transkey])
-    dloader = torch.utils.data.DataLoader(dset, batch_size=batch_size,
-                                          shuffle=shuffle, num_workers=4)
+    #train_v2.csv
+    dset = PlanetDataset(DATA_DIR+'/train_labels.csv', transform=data_transforms[transkey])
+    dloader = torch.utils.data.DataLoader(dset, batch_size=4, shuffle=shuffle, num_workers=4)
     dloader.num = dset.num
     return dloader
 
-
-def get_val_loader(model, batch_size=16, shuffle=True):
-    if model.name == 'inceptionv3':
+def get_val_loader(model, batch_size = 16, shuffle = True):
+    if model.name.startswith('inception'):
         transkey = 'validv3'
     else:
         transkey = 'valid'
     if hasattr(model, 'batch_size'):
         batch_size = model.batch_size
-    # train_v2.csv
-    dset = PlanetDataset(DATA_DIR + '/train_labels.csv', train_data=False,
-                         transform=data_transforms[transkey])
-    dloader = torch.utils.data.DataLoader(dset, batch_size=batch_size,
-                                          shuffle=shuffle, num_workers=4)
+    #train_v2.csv
+    dset = PlanetDataset(DATA_DIR+'/train_labels.csv', train_data=False, transform=data_transforms[transkey])
+    dloader = torch.utils.data.DataLoader(dset,  batch_size=4, shuffle=shuffle, num_workers=4)
     dloader.num = dset.num
     return dloader
 
-
-def get_test_loader(model, batch_size=16, shuffle=False):
-    if model.name == 'inceptionv3':
+def get_test_loader(model, batch_size = 16, shuffle = False):
+    if model.name.startswith('inception'):
         transkey = 'testv3'
     else:
         transkey = 'test'
     if hasattr(model, 'batch_size'):
         batch_size = model.batch_size
 
-    dset = PlanetDataset(DATA_DIR + '/sample_submission.csv', has_label=False,
-                         transform=data_transforms[transkey])
-    dloader = torch.utils.data.DataLoader(dset, batch_size=batch_size,
-                                          shuffle=shuffle, num_workers=4)
+    dset = PlanetDataset(DATA_DIR+'/sample_submission.csv', has_label=False, transform=data_transforms[transkey])
+    dloader = torch.utils.data.DataLoader(dset,  batch_size=4, shuffle=shuffle, num_workers=4)
     dloader.num = dset.num
     return dloader
 

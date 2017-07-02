0e37d9cae514c1775075213a9d7e6777860b57e3
Xiaoliang Li <xiaoliangl@vmware.com>
2017-07-02 19:55:36 +0800

    refactor batchsize to setting

--- species2/settings.py
+++ species2/settings.py
@@ -3,3 +3,4 @@ DATA_DIR = '/home/caitsithx/deeplearning_workspace/data/invasive-species' \
 TRAIN_DIR = DATA_DIR + '/train-640'
 TEST_DIR = DATA_DIR + '/test-640'
 MODEL_DIR = DATA_DIR + '/models'
+BATCH_SIZE = 24
--- species2/train.py
+++ species2/train.py
@@ -1,39 +1,26 @@
-import settings
+import argparse
 
+import time
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
-import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
-import time
-import copy
-import os
-import cv2
-import random
-import argparse
-import bcolz
-import pandas as pd
-import random
-from PIL import Image
 
-from utils import save_array, load_array, save_weights, load_best_weights, w_files_training
-from utils import create_dense161, create_dense201, create_res101, create_res152, create_dense169, create_res50
-from utils import create_vgg19bn, create_vgg16bn, create_dense121, create_inceptionv3
-from utils import create_model
 from cscreendataset import get_train_loader, get_val_loader
+from settings import *
+from utils import create_model
+from utils import save_weights, load_best_weights, w_files_training
 
-data_dir = settings.DATA_DIR
+RESULT_DIR = DATA_DIR + '/results'
 
-RESULT_DIR = data_dir + '/results'
-MODEL_DIR = settings.MODEL_DIR
 batch_size = 8
 epochs = 100
 
-def train_model(model, criterion, optimizer, lr_scheduler, max_num = 2, init_lr=0.001, num_epochs=100):
-    data_loaders = { 'train': get_train_loader(model), 'valid': get_val_loader(model)}
+
+def train_model(model, criterion, optimizer, lr_scheduler, max_num=2,
+                init_lr=0.001, num_epochs=100):
+    data_loaders = {'train': get_train_loader(model, batch_size=BATCH_SIZE),
+                    'valid': get_val_loader(model, batch_size=BATCH_SIZE)}
 
     since = time.time()
     best_model = model
@@ -53,21 +40,22 @@ def train_model(model, criterion, optimizer, lr_scheduler, max_num = 2, init_lr=
             running_corrects = 0
             for data in data_loaders[phase]:
                 inputs, labels, _ = data
-                inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+                inputs, labels = Variable(inputs.cuda()), Variable(
+                    labels.cuda())
                 optimizer.zero_grad()
                 outputs = model(inputs)
-                #preds = torch.sigmoid(outputs.data)
+                # preds = torch.sigmoid(outputs.data)
                 preds = torch.ge(outputs.data, 0.5).view(labels.data.size())
-                #_, preds = torch.max(outputs.data, 1)
+                # _, preds = torch.max(outputs.data, 1)
                 loss = criterion(outputs, labels)
                 if phase == 'train':
                     loss.backward()
                     optimizer.step()
                 running_loss += loss.data[0]
                 running_corrects += torch.sum(preds.int() == labels.data.int())
-            print ("running_loss %d" % running_loss)
-            print ("running_corrects %d" % running_corrects)
-            print ("data_num %d" % data_loaders[phase].num)
+            print("running_loss %d" % running_loss)
+            print("running_corrects %d" % running_corrects)
+            print("data_num %d" % data_loaders[phase].num)
             epoch_loss = running_loss / data_loaders[phase].num
             epoch_acc = running_corrects / data_loaders[phase].num
 
@@ -78,9 +66,9 @@ def train_model(model, criterion, optimizer, lr_scheduler, max_num = 2, init_lr=
                 save_weights(epoch_acc, model, epoch, max_num=max_num)
             if phase == 'valid' and epoch_acc > best_acc:
                 best_acc = epoch_acc
-                #best_model = copy.deepcopy(model)
-                #torch.save(best_model.state_dict(), w_file)
-        print('epoch {}: {:.0f}s'.format(epoch, time.time()-epoch_since))
+                # best_model = copy.deepcopy(model)
+                # torch.save(best_model.state_dict(), w_file)
+        print('epoch {}: {:.0f}s'.format(epoch, time.time() - epoch_since))
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
@@ -88,9 +76,10 @@ def train_model(model, criterion, optimizer, lr_scheduler, max_num = 2, init_lr=
     print(w_files_training)
     return best_model
 
+
 def lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=7):
     """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
-    lr = init_lr * (0.6**(epoch // lr_decay_epoch))
+    lr = init_lr * (0.6 ** (epoch // lr_decay_epoch))
 
     if epoch % lr_decay_epoch == 0:
         print('LR is set to {}'.format(lr))
@@ -99,6 +88,7 @@ def lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=7):
         print('existing lr = {}'.format(param_group['lr']))
         param_group['lr'] = lr
     return optimizer
+
 
 def cyc_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=6):
     lr = 0
@@ -114,14 +104,16 @@ def cyc_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=6):
             param_group['lr'] = lr
     return optimizer
 
-def train(model, init_lr = 0.001, num_epochs = epochs):
+
+def train(model, init_lr=0.001, num_epochs=epochs):
     criterion = nn.BCELoss()
     # Observe that all parameters are being optimized
     optimizer_ft = optim.SGD(model.parameters(), lr=init_lr, momentum=0.9)
-    #optimizer_ft = optim.Adam(model.parameters(), lr=init_lr)
+    # optimizer_ft = optim.Adam(model.parameters(), lr=init_lr)
 
-    model = train_model(model, criterion, optimizer_ft, cyc_lr_scheduler, init_lr=init_lr,
-                        num_epochs=num_epochs, max_num = model.max_num)
+    model = train_model(model, criterion, optimizer_ft, cyc_lr_scheduler,
+                        init_lr=init_lr,
+                        num_epochs=num_epochs, max_num=model.max_num)
     return model
 
 
@@ -135,6 +127,7 @@ def train_net(model_name):
     if not hasattr(model, 'max_num'):
         model.max_num = 2
     train(model)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--train", nargs=1, help="train model")
